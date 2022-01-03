84c6c5b7bc6714b42c7032f2ab5405435f9b47c8
Snarfy <snarfy@nifty-league.com>
2022-01-03 11:41:37 -0800

    Add Photon-Quantum-Sdk_v2.1.0-RC1 files

---
+++ Photon-Quantum-Sdk_v2.1.0-RC1/quantum_code/quantum.code/CommandSetup.cs
@@ -0,0 +1,14 @@
+﻿using Photon.Deterministic;
+
+namespace Quantum {
+  public static class CommandSetup {
+    public static DeterministicCommand[] CreateCommands(RuntimeConfig gameConfig, SimulationConfig simulationConfig) {
+      return new DeterministicCommand[] {
+        // pre-defined core commands
+        Core.DebugCommand.CreateCommand(),
+
+        // user commands go here
+      };
+    }
+  }
+}
